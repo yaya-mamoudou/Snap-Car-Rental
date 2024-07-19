20fec38f5cd10b9cd4f57403e0155da734e04734
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-07-19 20:19:15 +0100

    fix gap issue

--- src/components/pages/home/our-fleet.tsx
+++ src/components/pages/home/our-fleet.tsx
@@ -22,7 +22,7 @@ export default function OurFleet() {
     <div className="bg-[#DBDBDB] py-10">
       <div className="container my-14 flex flex-col items-center">
         <h2 className="text-center text-2xl font-semibold">Our Fleet</h2>
-        <div className="mt-10 grid w-full grid-cols-12 gap-10 lg:w-3/4">
+        <div className="mt-10 grid w-full grid-cols-12 gap-x-0 gap-y-10 sm:gap-x-10 lg:w-3/4">
           {cars.map((item, key) => (
             <div key={key} className="col-span-12 sm:col-span-6 2xl:col-span-4">
               <CarCard {...item} />
