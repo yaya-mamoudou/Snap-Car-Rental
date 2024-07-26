935457bd7b82a78de376b6d930f59f308c045fc1
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-07-26 02:46:56 +0100

    confirm page

--- src/app/(client)/cars/[car-slug]/confirm/page.tsx
+++ src/app/(client)/cars/[car-slug]/confirm/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import Image from "next/image";
+import Button from "~/components/common/button";
+import CarCard from "~/components/common/car-card";
+import { cars } from "~/components/pages/home/data/mock";
 import ReservationSteps from "~/components/ui/reservation-steps";
+const car = cars(1)[0];
 
 export default function Page() {
   return (
@@ -9,9 +13,66 @@ export default function Page() {
         <div className="container">
           <h1 className="mb-10 text-2xl font-semibold">Confirm Booking</h1>
 
-          <div></div>
+          <div className="grid grid-cols-12 gap-x-10 gap-y-10">
+            <div className="col-span-12 md:col-span-8">
+              <CarCard horizontal {...car} />
+            </div>
+            <div className="col-span-12 md:col-span-4">
+              <div className="rounded-xl bg-white py-4">
+                <h2 className="px-4 text-lg font-semibold lg:px-10">
+                  Pricing Details
+                </h2>
+                <div>
+                  {data.map((item) => (
+                    <div
+                      className="flex justify-between px-4 py-2 text-sm odd:bg-gray-100 lg:px-10"
+                      key={item.label}
+                    >
+                      <span className="text-black/40">{item.label}</span>
+                      <span className="font-semibold">{item.amount}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <h5 className="mt-10 text-lg font-medium">
+            We accept the following payment methods:
+          </h5>
+
+          <div className="flex flex-wrap gap-10">
+            {paymentData.map((item) => (
+              <Image
+                src={item}
+                key={item}
+                alt="Payment option"
+                width={120}
+                height={70}
+                className="object-contain"
+              />
+            ))}
+          </div>
+
+          <Button className="mt-10 w-full">Confirm</Button>
         </div>
       </div>
     </div>
   );
 }
+
+const data = [
+  { label: "Rental Fee/Day", amount: "$12" },
+  { label: "Insurance", amount: "$12" },
+  { label: "Tax", amount: "$6" },
+  { label: "Total Cost", amount: "$28" },
+];
+
+const paymentData = [
+  "/images/payment/Cash App.webp",
+  "/images/payment/Paypal.webp",
+  "/images/payment/Square.webp",
+  "/images/payment/Stripe.webp",
+  "/images/payment/Venmo.webp",
+  "/images/payment/zelle.png",
+];
--- src/components/common/car-card.tsx
+++ src/components/common/car-card.tsx
@@ -3,29 +3,46 @@ import { BriefcaseBusiness, Settings2, User } from "lucide-react";
 import Image from "next/image";
 import { cars } from "../pages/home/data/mock";
 import Button from "./button";
+import { cn } from "@nextui-org/react";
 const carsList = cars()[1];
 
-const CarCard = (props: typeof carsList) => {
+type Props = Partial<typeof carsList> & {
+  horizontal?: boolean;
+};
+const CarCard = (props: Props) => {
   props?.images;
 
   return (
-    <div className="rounded-xl bg-white p-2">
-      <div className="aspect-3/2 rounded-lg">
+    <div
+      className={cn(
+        "flex flex-col rounded-xl bg-white p-2",
+        props.horizontal && "md:flex-row md:gap-x-4",
+      )}
+    >
+      <div
+        className={cn(
+          "aspect-3/2 rounded-lg",
+          props.horizontal && "md:w-[200px]",
+        )}
+      >
         <Carousel
           slide={false}
           rightControl={<div></div>}
           leftControl={<div></div>}
+          indicators={!props.horizontal}
         >
-          {props?.images.map((img, key) => (
-            <Image
-              key={key}
-              alt={props?.name}
-              src={img}
-              width={200}
-              height={200}
-              className="size-full object-cover"
-            />
-          ))}
+          {props?.images
+            ?.slice(0, props.horizontal ? 1 : undefined)
+            ?.map((img, key) => (
+              <Image
+                key={key}
+                alt={props?.name ?? ""}
+                src={img}
+                width={200}
+                height={200}
+                className="size-full object-cover"
+              />
+            ))}
         </Carousel>
       </div>
       <div className="mt-5 pl-2">
@@ -33,11 +50,12 @@ const CarCard = (props: typeof carsList) => {
         <div className="mt-2 text-tiny font-semibold text-gray-500">
           <div>
             <span>
-              {props?.price.daily}/<span className="text-[0.6rem]">Day</span>
+              {props?.price?.daily}/<span className="text-[0.6rem]">Day</span>
             </span>
             <span className="mx-2">|</span>
             <span>
-              {props?.price.month}/ <span className="text-[0.6rem]">Month</span>
+              {props?.price?.month}/{" "}
+              <span className="text-[0.6rem]">Month</span>
             </span>
           </div>
           <div className="mt-2">{props?.engine} Engine</div>
@@ -55,9 +73,15 @@ const CarCard = (props: typeof carsList) => {
           </div>
         </div>
       </div>
-      <Button link href={`/cars/${props?.id}/preview`} className="mt-6 w-full">
-        Rent Now
-      </Button>
+      {!props.horizontal && (
+        <Button
+          link
+          href={`/cars/${props?.id}/preview`}
+          className="mt-6 w-full"
+        >
+          Rent Now
+        </Button>
+      )}
     </div>
   );
 };
