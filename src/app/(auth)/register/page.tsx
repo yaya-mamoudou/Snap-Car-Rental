4c49966b9a8a4e77b16c5ba3aa9ae8f3a7a6fae8
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-07-29 21:23:39 +0100

    add padding

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "~/components/pages/register/form";
 export default function Page() {
 
     return (
-        <div className="container pt-10 flex justify-center items-center flex-col">
+        <div className="container py-10 flex justify-center items-center flex-col">
             <div className="w-full md:w-4/5  lg:w-3/5">
                 <h1 className="text-2xl text-white font-semibold">Register</h1>
                 <div className="bg-white relative rounded-lg p-10 mt-10">
