ea17912e8465ab442cc86870682c2db42adc8d31
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2024-09-13 21:46:28 +0100

    Add seperate form validation schema for register and login forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,14 @@
 import LoginForm from "~/components/pages/login/form";
 
 export default function Page() {
-
-    return (
-        <div className="container pt-10 flex justify-center items-center flex-col">
-            <div className="w-full md:w-2/3  lg:w-2/5">
-                <h1 className="text-2xl text-white font-semibold">Login</h1>
-                <div className="bg-white rounded-lg p-10 mt-10">
-                    <LoginForm />
-                </div>
-            </div>
+  return (
+    <div className="container flex flex-col items-center justify-center pt-10">
+      <div className="w-full md:w-2/3 lg:w-2/5">
+        <h1 className="text-2xl font-semibold text-white">Login</h1>
+        <div className="mt-10 rounded-lg bg-white p-10">
+          <LoginForm />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import * as yup from 'yup'
 
 export enum BookingStatus {
     PAID = 'Paid',
@@ -9,7 +10,7 @@ export enum BookingStatus {
 
 export const loginSchema = z.object({
     email: z.string(),
-    password: z.string()
+    password: z.string().min(8, 'Password must be at least 8 characters')
 })
 
 export const signupSchema = z.object({
@@ -19,6 +20,39 @@ export const signupSchema = z.object({
     username: z.string(),
     gender: z.enum(['MALE', 'FEMALE', 'OTHER']),
     drivers_lisence: z.string(),
-    password: z.string(),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
     insurance: z.string(),
+});
+
+const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
+const MAX_FILE_SIZE = 1024 * 1024 * 2; // 5MB
+
+const fileSchema =
+    yup.mixed<File>()
+        .test('fileSize', 'File size is too large', (file) => {
+            return file && file.size <= MAX_FILE_SIZE;
+        })
+        .test('fileType', 'Unsupported file type', (file) => {
+            return file && SUPPORTED_FORMATS.includes(file.type);
+        })
+        .required('At least one file is required')
+
+
+export const signupFormSchema = yup.object({
+    fullname: yup.string().required(),
+    email: yup.string().email("This is not a valid email").required(),
+    phone_number: yup.number().required(),
+    username: yup.string().required(),
+    gender: yup.string().oneOf(['MALE', 'FEMALE', 'OTHER']).required(),
+    drivers_lisence: fileSchema,
+    insurance: fileSchema,
+    password: yup.string().min(8, "Password must be at least 8 characters long").required(),
+    password_confirmation: yup.string()
+        .oneOf([yup.ref('password'), ''], 'Passwords must match')
+        .required('Password confirmation is required'),
+});
+
+export const loginFormSchema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).required()
 })
